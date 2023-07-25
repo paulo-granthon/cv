35abf34fed673168f5bdf9c7e15323037a5c747c
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-07-25 18:13:15 -0300

    chore[gh-fetch]: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import { useState } from 'react'
-
-import Heading from "./components/Heading"
-import Section from "./components/Section"
-import List from "./components/List"
+import { useState, useEffect } from 'react';
+import { fetchGitHubProfilePicture } from './services/GitHubService';
 
 import { Profile } from "./components/Profile"
 
 import { Data as dataSchema } from "./schemas/Data";
 
 function App() {
-
     const { profile } = dataSchema;
+    const [profilePicture, setProfilePicture] = useState<string | null>(null);
 
+    useEffect(() => {
+        // Fetch the GitHub profile picture when the component mounts
+        fetchGitHubProfilePicture()
+        .then((avatarUrl) => setProfilePicture(avatarUrl))
+        .catch((error) => console.error('Error fetching profile picture:', error));
+    }, []);
+    
     return (
         <>
             <main className="l-main bd-container" id="bd-container">
                 <div className="resume" id="area-cv">
                     <div className="resume__left">
-                        <Profile {...profile} />
+                        {profilePicture !== null ? (
+                            <Profile {...profile} image={profilePicture} />
+                        ) : (
+                            <p>Loading profile...</p>
+                        )}
                     </div>
                     <div className="resume__left">
 
