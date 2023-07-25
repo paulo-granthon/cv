6d5a612fb6650efe75442ac257358ea4a1bf7cad
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-07-25 20:11:09 -0300

    add[gh-fetch]: repo languages

--- src/services/GitHubService.ts
+++ src/services/GitHubService.ts
@@ -26,3 +26,27 @@ export async function fetchGitHubProfilePicture() {
     }
 }
 
+export async function fetchGitHubRepositoriesLanguages() {
+    try {
+        const githubUsername = import.meta.env.VITE_GITHUB_USERNAME;
+        const githubToken = import.meta.env.VITE_GITHUB_TOKEN;
+
+        const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${githubUsername}/repos`, {
+            headers: {
+                Authorization: `token ${githubToken}`,
+            },
+        });
+
+        const languages: string[] = [];
+        response.data.forEach((repo: any) => {
+            if (repo.language && !languages.includes(repo.language)) {
+                languages.push(repo.language);
+            }
+        });
+
+        return languages;
+    } catch (error) {
+        console.error('Error fetching GitHub repositories languages:', error);
+        return [];
+    }
+}
