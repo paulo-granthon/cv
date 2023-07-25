eb07da8e849b4c63c6c498b118f0be74268d4cf1
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-07-25 20:11:29 -0300

    add: App.tsx hard skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
-import { fetchGitHubProfilePicture } from './services/GitHubService';
+import {
+    fetchGitHubProfilePicture,
+    fetchGitHubRepositoriesLanguages
+} from './services/GitHubService';
 
 import { Profile } from "./components/Profile"
 import { Skills } from "./components/Skills"
@@ -10,14 +13,22 @@ import { Data as dataSchema } from "./schemas/Data";
 import "./App.css";
 
 function App() {
-    const { profile, about, softSkills } = dataSchema;
+    const { profile, about, hardSkills, softSkills } = dataSchema;
     const [profilePicture, setProfilePicture] = useState<string | null>(null);
+    const [hardSkills, setHardSkills] = useState<string[]>([]);
 
     useEffect(() => {
+
         // Fetch the GitHub profile picture when the component mounts
         fetchGitHubProfilePicture()
-        .then((avatarUrl) => setProfilePicture(avatarUrl))
-        .catch((error) => console.error('Error fetching profile picture:', error));
+            .then((avatarUrl) => setProfilePicture(avatarUrl))
+            .catch((error) => console.error('Error fetching profile picture:', error));
+
+        // Fetch the hard skills from GitHub repositories when the component mounts
+        const { githubUsername, githubToken } = dataSchema;
+        fetchGitHubRepositoriesLanguages(githubUsername, githubToken)
+            .then((languages) => setHardSkills(languages))
+            .catch((error) => console.error('Error fetching GitHub repositories languages:', error));
     }, []);
     
     return (
@@ -30,6 +41,9 @@ function App() {
                             <Profile {...profile} image={profilePicture} />
                         ) : (
                             <p>Loading profile...</p>
+                        )}
+                        {hardSkills.length > 0 && (
+                            <Skills title="Hard Skills" items={hardSkills} />
                         )}
                         <Skills {...softSkills} />
                     </div>
