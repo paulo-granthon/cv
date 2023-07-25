8ccd60f840c75f2596ebe6da475cf74b8bf94f44
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-07-25 18:50:56 -0300

    chore: update Data.ts

--- src/schemas/Data.ts
+++ src/schemas/Data.ts
@@ -5,5 +5,9 @@ export const Data = {
         location: "São José dos Campos, São Paulo",
         email: "pg.granthon@gmail.com",
         telephone: "+55 22 98124 9249",
+    },
+    about: {
+        label: "Perfil",
+        description: "Experienced Fullstack Developer skilled in web and desktop app development using various languages and frameworks. Proficient in Agile methodologies, delivering efficient solutions. Strong focus on code quality, adhering to industry standards, and innovative problem-solving. Passionate about staying ahead of tech trends and thriving in excellence-driven teams."
     }
 }
