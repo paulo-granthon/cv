98b1d9b7223cbb0b32c948eeaca2b1e34ee7741c
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-07-25 13:29:53 -0300

    chore: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Heading from "./components/Heading"
-import { Section } from "./components/Section"
+import Section from "./components/Section"
 import List from "./components/List"
 
 import { useState } from 'react'
@@ -7,9 +7,18 @@ import { useState } from 'react'
 function App() {
     return (
         <>
-            <Heading title={"Hello"} />
-            <Section title={"This is a section"}>oi</Section>
-            <List items={["1", "2", "3"]} render={(item: string) => <span className="gold">{item}</span>} />
+            <main className="l-main bd-container" id="bd-container">
+                <div className="resume" id="area-cv">
+                    <div className="resume__left">
+                        <Heading title={"Hello"} />
+                        <Section title={"This is a section"}>oi</Section>
+                        <List items={["1", "2", "3"]} render={(item: string) => <span className="gold">{item}</span>} />
+                    </div>
+                    <div className="resume__left">
+
+                    </div>
+                </div>
+            </main>
         </>
     )
 }
