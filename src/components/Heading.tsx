45d6a4e45d57e2aeb2038a5545f2120c60af01cb
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-07-25 18:01:42 -0300

    remove: components Heading, Options, Section

--- src/components/Heading.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from "react"
-
-type HeadingProps = { title: string }
-
-const Heading = ({ title }: HeadingProps): ReactElement => {
-    return <h1>{title}</h1>
-}
-export default Heading
