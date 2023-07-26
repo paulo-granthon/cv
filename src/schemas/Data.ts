d85b3682bb24431570dc01a5192f787b943b1ea9
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-07-26 17:21:13 -0300

    chore: update Data.ts

--- src/schemas/Data.ts
+++ src/schemas/Data.ts
@@ -11,9 +11,10 @@ export const Data = {
     },
     about: {
         label: "About",
-        description: "Experienced Fullstack Developer skilled in web and desktop development using various languages and frameworks. Proficient in Agile methodologies, delivering efficient solutions. Strong focus on code quality and innovative solutions. Passionate about staying ahead of tech trends and thriving in excellence-driven teams.",
+        description: "Experienced Fullstack Developer skilled in web and desktop development using various languages and frameworks. Proficient in Agile methodologies, delivering efficient solutions. Strong focus on code quality and innovative solutions. Passionate about staying ahead of tech trends and thriving in challenging tasks.",
     },
     hardSkills: [
+        "Git & GitHub",
         "CI/CD",
         "Docker",
         "SQL",
@@ -23,17 +24,16 @@ export const Data = {
         "Django",
         "Rocket",
         "Diesel",
-        "JavaFX",
     ],
     languages: {
         items: [
             "C++",
         ],
     },
-    languagesAlias: {
+    languageAliases: {
         Shell: "Bash",
         ShaderLab: "HLSL",
-    } as LanguagesAlias,
+    } as LanguageAliases,
     hideLanguages: [
         "Svelte",
         "Dockerfile",
@@ -58,10 +58,56 @@ export const Data = {
             period: "Aug. 2022 - Jul. 2023",
             company: "GWCloud",
             description: [
-                "I participated in the development and maintenance of information systems for external clients of GWCloud, alongside an Agile team.",
-                "My main tools were ServiceNow, JavaScript, and HTML. My work included both the Back End and Front End of each project, making it a Full Stack developer position.",
-                "During my time at GWCloud, I took part in the development of an HR platform for the Globo television network, which was awarded the \"Operational Excellence\" classification in the broadcaster's Partners in Excellence program due to the quality of the product delivered by me and my team."
+                "Developed and maintained information systems for external clients of GWCloud.",
+                "My work included both the Back End and Front End using ServiceNow, JavaScript, and HTML as the main technologies.",
+                "Contributed to the development of an award-winning HR platform for the Globo television network."
             ],
         },
     ],
+    education: [
+        {
+            title: "Database Technology",
+            period: "Aug. 2022 - Jul. 2025",
+            institution: "FATEC Prof. Jessen Vidal",
+            description: [
+                "I Develop tailored solutions for challenges proposed by real companies each semester using Scrum methodology.",
+                "Served as Product Owner of the team in my second project, leading the team to deliver the best project and earning recognition, participating in an internal meeting with the company to present our solution for their employee work schedule management needs.",
+            ],
+        },
+    ],
+    projects: [
+        {
+            title: "Online Curriculum Vitae | React + TypeScript",
+            url: "cv",
+            additionalDesc: "This curriculum. Fetches my profile picture, languages and additional info for the projects on the portfolio section from my GitHub profile.",
+            additionalTech: [
+                "React",
+                "Docker",
+            ]
+        },
+        {
+            title: "RustyTetris | Rust",
+            url: "rusty-tetris",
+            additionalDesc: "",
+            additionalTech: [],
+        },
+        {
+            title: "TheToDoOffice | Django + HTMX",
+            url: "TheToDoOffice",
+            additionalDesc: "",
+            additionalTech: [],
+        },
+        {
+            title: "Khali | Tkinter",
+            url: "Khali",
+            additionalDesc: "",
+            additionalTech: [],
+        },
+        {
+            title: "API2Semestre | JavaFX",
+            url: "API2Semestre",
+            additionalDesc: "",
+            additionalTech: [],
+        },
+    ] as ProjectData[],
 }
