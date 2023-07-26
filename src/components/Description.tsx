977c04ca3ad818ac20c28c3c8a0ab73890965f4e
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-07-26 17:18:04 -0300

    refactor: moved component Description

---
+++ src/components/Description.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+interface DescriptionProps {
+    desc: string,
+}
+
+export const Description = ({ desc }: DescriptionProps): ReactElement => (
+    <p className="experience__description">{desc}</p>
+);
