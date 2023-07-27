dd5135dd31a46d952b058228b67c7bce2bcffbc5
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-07-27 01:54:15 -0300

    chore: update ListSection behavior

--- src/components/ListSection.tsx
+++ src/components/ListSection.tsx
@@ -17,25 +17,14 @@ interface ListSectionProps {
 }
 
 export const ListSection = ({ title, items }: ListSectionProps): ReactElement => {
-    const splitIndex = Math.ceil(items.length / 2);
-    const firstColumnItems = items.slice(0, splitIndex);
-    const secondColumnItems = items.slice(splitIndex);
-
     return (
         <>
             <section className="hard-items section" id="items">
                 <h2 className="section-title">{title}</h2>
                     <div className="items__content bd-grid">
-                        <div className="items__column">
-                            <ul className="items__data">
-                                {firstColumnItems.map((item) => <Item key={item} item={item} />)}
-                            </ul>
-                        </div>
-                        <div className="items__column">
-                            <ul className="items__data">
-                                {secondColumnItems.map((item) => <Item key={item} item={item} />)}
-                            </ul>
-                        </div>
+                        <ul className="items__data">
+                            {items.map((item) => <Item key={item} item={item} />)}
+                        </ul>
                     </div>
             </section>
         </>
