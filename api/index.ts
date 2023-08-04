9a54e824b9ca61cac2c47b51178d80e437be75fa
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-04 02:12:41 -0300

    fix: backend integration / Docker / docker-compose

---
+++ api/index.ts
@@ -0,0 +1,21 @@
+import express from'express';
+import { Request, Response } from 'express';
+import { config as dotenvConfig } from 'dotenv';
+// import cors from 'cors';
+
+const app = express();
+
+dotenvConfig();
+
+const port = process.env.PORT || 4000;
+
+// app.use(cors());
+app.use(express.json());
+
+app.get('/message', (req: Request, res: Response) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
