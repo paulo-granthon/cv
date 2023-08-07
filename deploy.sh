009142734721da95891e8c24a6c2b8f8d854f6d8
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-07 19:27:58 -0300

    ci: update ci/cd files

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,7 @@ cd ../api
 npm install
 cd ..
 
-sudo docker-compose build
-sudo docker-compose down
-sudo docker-compose up -d
+sudo docker-compose up --build -d
 
 sudo systemctl restart nginx
 sudo pm2 restart all
