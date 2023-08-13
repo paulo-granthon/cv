b4f2913ab053b65630736317b7199b4f095d6d13
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-08-13 08:04:06 -0300

    build: aws amplify

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
+    plugins: [react()],
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        host: true, // needed for the Docker Container port mapping to work
+            strictPort: true,
+        port: 5173,
     },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 5173,
-  }
+    resolve: {
+        alias: {
+            "./runtimeConfig": "./runtimeConfig.browser",
+        },
+    },
 })
